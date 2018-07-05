74e35cd8b1fb83af199004dd750847d970e35599
TPEmist <tuchengyin0916@gmail.com>
2018-07-06 06:24:21 +0800

    basic build

---
+++ src/swiftpro/include/swiftpro/swiftpro_control.h
@@ -0,0 +1,10 @@
+#ifndef SWIFTPRO_CONTROL
+#define SWIFTPRO_CONTROL
+
+#include <swiftpro/position.h>
+#include <swiftpro/SwiftproState.h>
+#include <swiftpro/status.h>
+#include <swiftpro/position.h>
+#include <swiftpro/angle4th.h>
+
+#endif
---
+++ src/swiftpro/src/swiftpro_control.cpp
@@ -0,0 +1,8 @@
+#include "swiftpro_control.h"
+
+#include <ros/ros.h>
+#include <stdlib.h>
+
+int main(){
+	return 1;
+}
